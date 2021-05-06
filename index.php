171fbd81e2cf388d8f9baf691a4bd5ecfd430f1a
JaimeAndresLopez <james_2005_scott@hotmail.coml>
2021-05-06 18:00:15 -0500

    Ciclos_for_06-05-2021

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+   // $nombre = (string) "Jaime";
+    //var_dump(isset($nombre));//isset se utiliza para verificar q una VARIABLE existe y q no sea null
+    //is_null se utiliza para verificar si una VARIABLE existe y su tipo de dato y dato en null
+    //Null se representa de manera diferente en cada tipo de dato ej: int Null es 0, en String es un vacio y en Bool es un false
+    // empty es para verficar q una VARIABLE exista pero q su dato sea vacio
+     //ciclos
+     // $i es la inicializacion, luego hasta donde basado en la condicion y por ultimo el avanza.
+    
+   // for ($i=0; $i < 5; $i++) { 
+   //      echo "$i";
+   //  }
+
+//array, es un tipo de dato como una lista, su posicion inicial es 0
+$lista = (array) ["Jaime","Andres", null, "Lopez"];
+//                 0     ,   1    ,  2 ,    3    pero la lista tiene 4  datos
+// count es una funcion para contar los datos de los array 
+// echo count($lista); mostraria que hay 4 datos
+// el continue se utiliza para omitir, el  break para romper y salir y el  goto para saltar a otra linea
+?>
